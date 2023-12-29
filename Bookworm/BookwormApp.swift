122bc5da53e51d4fbefd7613e2c32d8253148039
Daniel Yankiver <dyankiver@gmail.com>
2023-12-29 13:59:30 -0500

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Daniel Yankiver on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bookworm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Daniel Yankiver on 12/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
