7a398cd77ad3c60023e924d59dfcdc5a690f4eb1
Daniel Yankiver <dyankiver@gmail.com>
2023-12-30 11:09:03 -0500

    creating books with SwiftData

--- Bookworm/BookwormApp.swift
+++ Bookworm/BookwormApp.swift
@@ -5,13 +5,15 @@
 //  Created by Daniel Yankiver on 12/29/23.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
 struct BookwormApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+    .modelContainer(for: Book.self)
+  }
 }
